e1a79920ae4eaf762c63a85bebb052946fc0b554
Raíssa Regis <rclraissa@gmail.com>
2022-11-20 13:36:41 -0300

    feat: created methods and configuration for the attendance module

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -1,16 +1,65 @@
-import { Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwtAuth.guard';
+import { RolesGuard, Roles, Role } from 'src/auth/roles';
+import { ITaskService } from 'src/task/interfaces/ITaskService.interface';
+import { createAttendanceDto } from './dtos/createAttendance.dto';
+import { Attendance } from './entities/Attendance.entity';
 import { IAttendanceService } from './interfaces/IAttendanceService.interface';
 
 @Controller('attendance')
 export class AttendanceController {
   constructor(
     @Inject('ATTENDANCE_SERVICE')
-    private readonly attendanceService: IAttendanceService
+    private readonly attendanceService: IAttendanceService,
+    @Inject('TASK_SERVICE')
+    private readonly taskService: ITaskService,
   ) { }
 
-
+  @UseGuards(JwtAuthGuard)
   @Post()
-  async getAttendance() {
+  async createAttendance(@Req() req, @Body() { professional, tasks }: createAttendanceDto) {
 
+    const client = req.user;
+
+    const readyTasks = await this.taskService.createTasks(client.name, professional, tasks);
+    const attendance = await this.attendanceService.createAttendance(client.name, professional, readyTasks);
+
+    const partialAttendance = new Attendance(attendance).client
+
+    return partialAttendance;
   }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Get('/startattendance/:id')
+  async startAttendance(@Param('id') id: string) {
+
+    const attendance = await this.attendanceService.startAttendance(id);
+
+    const partialAttendance = new Attendance(attendance);
+
+    return {
+      msg: "Attendance started",
+      partialAttendance,
+    };
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Get('stopattendance/:id')
+  async stopAttendance(@Param('id') id: string) {
+
+    const attendance = await this.attendanceService.stopAttendance(id);
+
+    attendance.totalComission
+
+    return {
+      msg: "Attendance and resume",
+      client: attendance.client,
+      duration: attendance.totalDuration,
+      commission: attendance.totalComission,
+    };
+  }
+
+
 }
--- src/attendance/attendance.service.ts
+++ src/attendance/attendance.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AppError } from 'src/errors/AppError.error';
+import { Task } from 'src/task/entities/Task.entity';
+import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
 import { Attendance } from './entities/Attendance.entity';
 import { IAttendanceService } from './interfaces/IAttendanceService.interface';
@@ -9,7 +12,68 @@ export class AttendanceService implements IAttendanceService {
 
   constructor(
     @InjectRepository(Attendance)
-    private readonly attendanceRepository: Repository<Attendance>
+    private readonly attendanceRepository: Repository<Attendance>,
+    private readonly userService: UserService,
   ) { }
+
+  async createAttendance(clientName: string, professionalName: string, tasks: Task[]): Promise<Attendance> {
+
+    const client = await this.userService.getClientByName(clientName);
+    const professional = await this.userService.getProfessionalByEmail(professionalName);
+
+    if (!client || !professional) throw new AppError("One or more accounts don't exist");
+
+    const totals = tasks.reduce((acc, cur) => {
+      acc.totalComission += cur.amountMinutes;
+      acc.totalDuration += cur.amountMinutes;
+
+      return acc;
+    }, {
+      totalDuration: 0,
+      totalComission: 0
+    });
+
+    const attendance = this.attendanceRepository.create({
+      client,
+      professional,
+      tasks,
+      totalDuration: totals.totalDuration,
+      totalComission: totals.totalComission
+    });
+
+    return attendance;
+  }
+
+  async findAttendanceById(id: string): Promise<Attendance> {
+    return await this.attendanceRepository.findOneBy({ id });
+  }
+
+  async startAttendance(id: string): Promise<Attendance> {
+    const attendance = await this.findAttendanceById(id);
+
+    if (!attendance) throw new AppError("Attendance doesn't exist");
+
+    if (attendance.isActive) throw new AppError("Attendance is already active");
+
+    Object.assign(attendance, {
+      isActive: true
+    });
+
+    return attendance;
+  }
+
+  async stopAttendance(id: string): Promise<Attendance> {
+    const attendance = await this.findAttendanceById(id);
+
+    if (!attendance) throw new AppError("Attendance doesn't exist");
+
+    if (!attendance.isActive) throw new AppError("Attendance is already over");
+
+    Object.assign(attendance, {
+      isActive: false
+    });
+
+    return attendance;
+  }
 
 }
