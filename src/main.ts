fc94c29771d017db53cc5a4ce8e2bfab8abd384a
Raíssa Regis <rclraissa@gmail.com>
2022-11-20 13:43:01 -0300

    chore: updated the main file

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,36 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = 3000;
-  const PREFIX = 'api';
+  const GLOBALPREFIX = 'api';
 
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix(PREFIX);
+  app.setGlobalPrefix(GLOBALPREFIX);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Mesha Clinic Documentation')
+    .setDescription("Mesha Clinic's API")
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+  });
+
+  SwaggerModule.setup(`${GLOBALPREFIX}/swagger`, app, document);
 
   await app.listen(PORT, () => {
-    console.log(`Server's running on: http://localhost:${PORT}`);
-
+    console.log(`Server's running on: http://localhost:${PORT}/${GLOBALPREFIX}`);
+    console.log(`Swagger documentation is running on: http://localhost:${PORT}/${GLOBALPREFIX}/swagger`);
   });
 
 }
