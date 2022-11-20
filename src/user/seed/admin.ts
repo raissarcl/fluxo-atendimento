63212313173b4b85cebfac458c1ea5792797f1b8
Raíssa Regis <rclraissa@gmail.com>
2022-11-20 07:57:46 -0300

    feat: created admin seed file

---
+++ src/user/seed/admin.ts
@@ -0,0 +1,36 @@
+import { hashSync } from "bcrypt";
+import { DataSource } from "typeorm";
+import { v4 as uuidv4 } from 'uuid';
+
+enum UsersTypeRoles {
+  CLIENT = "client",
+  PROFESSIONAL = "professional",
+  ADMIN = "admin",
+}
+
+async function createAdmin() {
+
+  const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "rootuser",
+    password: "admin",
+    database: "testem",
+  });
+
+  await AppDataSource.initialize();
+
+  await AppDataSource.query(
+    `INSERT INTO ATTENDANT(id, role, name, email, password, created_at, updated_at)
+    values ('${uuidv4()}', '${UsersTypeRoles.ADMIN}', 'admin', 'admin@admin.com', '${hashSync('admin', 8)}', 'now()', 'now()')
+    `
+  );
+
+  await AppDataSource.destroy();
+
+  console.log('Admin created.');
+}
+
+createAdmin();
+
